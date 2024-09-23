e40c855b074928f5ee2f8b4de9e1191385b5e801
ekamva <54266855+ekamva@users.noreply.github.com>
2024-09-23 11:52:42 +0300

    Increment-Decrement 2.2.4

--- calculations.js
+++ calculations.js
@@ -24,3 +24,18 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+
+// HW2.2.4
+
+function getRestforIncrement (x) {
+    x ++;
+    return x;
+  }
+
+function getResultforDecrement (y){
+    y --;
+    return y;
+}
+
+console.log(getRestforIncrement(76)); // 77
+console.log(getResultforDecrement(34)); // 33
