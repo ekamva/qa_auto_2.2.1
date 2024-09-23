4aec8cae360f36793dda8d2c97dba24a8f865440
ekamva <54266855+ekamva@users.noreply.github.com>
2024-09-23 10:41:55 +0300

    HW2.2.1

--- calculations.js
+++ calculations.js
@@ -3,15 +3,18 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
+// y was not defined
+function decreaseBalance(x, y) {
 x = x - y;
 return x;
 }
 
+// после return никакой код выполняться не будет
+
 function divideBalanceByAccounts(x, y) {
   x = x / y;
-  return x;
   console.log("x");
+  return x;
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +24,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2)); // в функции 2 параметра 
 console.log(getRestAfterDivision(7000, 3));
