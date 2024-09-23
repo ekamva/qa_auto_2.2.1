9199809e71050525b8d44478c0cf3289193eecae
ekamva <54266855+ekamva@users.noreply.github.com>
2024-09-23 10:51:20 +0300

    assignment operators 2.2.2

--- calculations.js
+++ calculations.js
@@ -1,28 +1,26 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
 // y was not defined
 function decreaseBalance(x, y) {
-x = x - y;
+x -= y;
 return x;
 }
 
-// после return никакой код выполняться не будет
-
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   console.log("x");
   return x;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2)); // в функции 2 параметра 
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
